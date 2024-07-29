b33a69503707b889995b32326058331a39c13b6f
Brian Ngetich <kipronobrian97@gmail.com>
2024-07-29 11:39:58 +0300

    Alembic

---
+++ app/migrations/versions/743a4ed5ebdf_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 743a4ed5ebdf
+Revises: 
+Create Date: 2024-07-29 11:29:17.048312
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '743a4ed5ebdf'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
